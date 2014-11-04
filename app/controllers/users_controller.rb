b60667e0cbd81bb7f2f28faa7906f8da4f697d46
Prince <prince.anim@meltwater.org>
2014-11-04 00:54:13 +0000

    Styled login page and sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
 
   private 
   def user_params
-    params.require(:user).permit(:firstname, :lastname, :email, :username, :password, :gender)
+    params.permit(:firstname, :lastname, :email, :username, :password, :gender)
   end
 
 end
